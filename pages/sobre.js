3679cbe6b3aee25abaf2b507e30df9763057ab90
Sérgio Luís de Azevedo Cruz Filho <sergioluisfilho@hotmail.com>
2021-03-16 00:20:27 -0300

    routing

---
+++ pages/sobre.js
@@ -0,0 +1,10 @@
+function Sobre(){
+    return (
+        <div>
+            <h1>Página Inicial</h1>
+            <a href="/Home">Acessar página Home</a>
+        </div>
+    )
+}
+
+export default Sobre
