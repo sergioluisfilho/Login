299fcaaaed8b5e67c9cd2a379f902e70b6f4a632
Sérgio Luís de Azevedo Cruz Filho <sergioluisfilho@hotmail.com>
2021-03-16 00:53:18 -0300

    index

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 function Home(){
     return (
         <div>
-            <h1>Página Inicial 3</h1>
+            <h1>Página Inicial</h1>
             <Link href="/sobre">
                 <a>clique para a página Sobre</a>
             </Link>
