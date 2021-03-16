9f825a4a727b832fbe4b9778e7d38ebd31ca0b3d
Sérgio Luís de Azevedo Cruz Filho <sergioluisfilho@hotmail.com>
2021-03-16 00:55:55 -0300

    index

--- pages/sobre.js
+++ pages/sobre.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 function Sobre(){
     return (
         <div>
-            <h1>Página Inicial</h1>
+            <h1>Página de sobre</h1>
             <Link href="/">
                 <a>clique para acessar página Home</a>
             </Link>
